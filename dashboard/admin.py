db65810d97090acbe28d6fb52f4f16acebbc21cd
mansijmaharzn <mansijmaharzn@gmail.com>
2024-05-31 12:50:45 +0545

    remoce wishlist

--- dashboard/admin.py
+++ dashboard/admin.py
@@ -1,6 +1,1 @@
 from django.contrib import admin
-
-from .models import Wishlist
-
-
-admin.site.register(Wishlist)
--- dashboard/views.py
+++ dashboard/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render, get_object_or_404, redirect
 from django.contrib.auth.decorators import login_required
 
 from recipe.models import Recipe
-from .models import Wishlist
 
 
 @login_required
@@ -12,23 +11,3 @@ def my_recipes(request):
     return render(request, 'dashboard/myrecipes.html', {
         'recipes': recipes
     })
-
-
-@login_required
-def my_wishlist(request):
-    wishlist = Wishlist.objects.filter(user=request.user)
-
-    return render(request, 'dashboard/mywishlist.html', {
-        'wishlist': wishlist[0].recipes.all()
-    })
-
-
-@login_required
-def add_to_wishlist(request, pk):
-    recipe = get_object_or_404(Recipe, pk=pk)
-
-    wishlist = Wishlist.objects.get_or_create(user=request.user)
-    wishlist[0].recipes.add(recipe)
-
-
-    return redirect('dashboard:my_recipes')
